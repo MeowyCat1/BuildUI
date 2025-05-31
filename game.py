b1457c153e1cabea3a9b0e8104793d7a7bf15336
MeowyCat1 <samhtfussey@gmail.com>
2025-05-31 12:09:35 +0100

    Added hangman game

---
+++ game.py
@@ -0,0 +1,80 @@
+from tkinter import *
+
+window = Tk()
+pressed = False
+strikes = 0
+
+
+def start():
+    global charlist
+    word = input("Enter the word: ")
+    charlist = list(word)
+    num = 0
+    charlistwithnum = []
+    for char in charlist:
+        charlistwithnum.append((char, num))
+        num += 1
+    charlist = charlistwithnum
+
+
+found_chars = []
+
+def check(letter):
+    global charlist
+    global found_chars
+    global strikes
+    for char in charlist:
+        if char[0] == letter and char[1] not in found_chars:
+            found_chars.append(char[1])
+        else:
+            strikes += 1
+            
+
+def make_string():
+    global charlist
+    global found_chars
+    string = ""
+    for char in charlist:
+        if char[1] in found_chars:
+            string = string + char[0]
+        else:
+            string = string + "_"
+    return string
+
+def check_win():
+    global charlist
+    global found_chars
+    won = True
+    while won == True:
+        for char in charlist:
+            if char[1] in found_chars:
+                won = True
+            else:
+                won = False
+                break
+            break
+    return won
+
+def run_gui():
+    global pressed
+    pressed = True
+
+        
+    
+
+entrybox = Entry(window)
+entrybox.grid(row=0, column= 0)
+button = Button(window, text= "Check", command= run_gui)
+button.grid(row= 0, column= 1)
+output = Label(window, text= "Waiting for output.")
+output.grid(row= 1, column= 0)
+strikesoutput = Label(window, text= "Strikes: ")
+strikesoutput.grid(row= 1, column= 1)
+
+start()
+while True:
+    check(input("Enter your letter: "))
+    print(make_string())
+
+
+window.mainloop()
